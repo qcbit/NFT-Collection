817a186dea65b9c5dab56fe8e175f723840bd087
Lance <lance@Lances-MBP.attlocal.net>
2022-10-15 08:33:52 -0500

    Added project files

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xA75eeF9ebCD842d7f9889D40A2eAc5c7ed8986A3";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
